5e8f75081c52ab5574391a54f786cf364930d47d
Namshima Iordye <mariostagehz@gmail.com>
2026-02-14 17:44:11 +0100

    Add UTF-8 encoding for environment file

--- reeana/api/config.py
+++ reeana/api/config.py
@@ -19,6 +19,7 @@ class Settings(BaseSettings):
     
     class Config:
         env_file = ".env"
+        env_file_encoding = 'utf-8'
 
 # Create a single instance to use everywhere
 settings = Settings()
